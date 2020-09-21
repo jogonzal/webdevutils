09e56a06589dbb8059b876ef549793d49fa596a1
Jorge <jogonzal@users.noreply.github.com>
2020-09-20 18:02:11 -0700

    Specific user

--- PosadationClient/src/components/Home.tsx
+++ PosadationClient/src/components/Home.tsx
@@ -1,12 +1,13 @@
 import { StackItem } from 'office-ui-fabric-react/lib/Stack'
 import { Text } from 'office-ui-fabric-react/lib/Text'
 import * as React from 'react'
-import { PrimaryButton } from 'office-ui-fabric-react'
+import { PrimaryButton, TextField } from 'office-ui-fabric-react'
 import * as shortId from 'shortid'
 import { AuthInfo } from '../shared/AuthInfo'
 import { VerticalStack } from './Stacks/VerticalStack'
 
 type State = {
+  userName: string
 }
 
 type Props = {
@@ -15,6 +16,9 @@ type Props = {
 export class Home extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
+    this.state = {
+      userName: '',
+    }
   }
 
   render() {
@@ -30,8 +34,8 @@ export class Home extends React.Component<Props, State> {
           <StackItem align='center'>
             <VerticalStack>
               <Text variant='xxLarge' style={ { paddingTop: '20px' } }>Posadation</Text>
-              <Text variant='medium'>Usuario: { currentUser }</Text>
-              <PrimaryButton href={`/#/playgame/${shortId.generate()}`}>Play game!</PrimaryButton>
+              <TextField placeholder='Username' value={ this.state.userName } onChange={ (_inp, val) => this.setState({ userName: val ?? '' }) } />
+              <PrimaryButton disabled={ this.state.userName === '' } href={`/#/playgame/${shortId.generate().substr(0, 5)}`} >Play game!</PrimaryButton>
             </VerticalStack>
           </StackItem>
         </VerticalStack>
--- PosadationClient/src/shared/AuthInfo.ts
+++ PosadationClient/src/shared/AuthInfo.ts
@@ -1,18 +1,27 @@
-import * as Faker from 'faker'
+// import * as Faker from 'faker'
 import { Log } from './logging/Log'
 
 export class AuthInfo {
   private static readonly userIdCacheKey = 'LocalUserId'
   public static getUserId(): string {
-    let userId = localStorage.getItem(this.userIdCacheKey)
+    const userId = localStorage.getItem(this.userIdCacheKey)
     if (userId) {
       Log.logger.info('Using user from local storage')
       return userId
     }
 
-    Log.logger.info('Creating a new user')
-    userId = Faker.name.firstName()
+    Log.logger.info('No user!')
+    throw new Error('No user!')
+    // userId = Faker.name.firstName()
+    // localStorage.setItem(this.userIdCacheKey, userId)
+    // return userId
+  }
+
+  public static setUserId(userId: string) {
+    if (!userId) {
+      throw new Error('Bad userId')
+    }
+
     localStorage.setItem(this.userIdCacheKey, userId)
-    return userId
   }
 }
