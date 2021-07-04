20e77cb3b0a9e2fd64eaa577fb4ade343b2974b5
Jorge <jogonzal@users.noreply.github.com>
2021-07-04 12:09:44 -0700

    Add step for failing build if pruning is needed

--- webdevclient/scripts/failIfPruningNeeded.ts
+++ webdevclient/scripts/failIfPruningNeeded.ts
@@ -5,3 +5,5 @@ printPruneInfo(pruneInfo)
 if (pruneInfo.modulesThatNeedPruning.length > 0) {
   throw new Error('There are modules that need pruning!')
 }
+// eslint-disable-next-line no-console
+console.log('Things are looking good!')
