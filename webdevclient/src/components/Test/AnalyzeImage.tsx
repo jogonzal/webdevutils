a71807f0d94ca41dc416c86bcfdcd1db00801ea5
Jorge <jogonzal@users.noreply.github.com>
2023-09-24 18:22:10 -0700

    Implement image parsing

--- webdevclient/src/components/Test/AnalyzeImage.tsx
+++ webdevclient/src/components/Test/AnalyzeImage.tsx
@@ -1,5 +1,63 @@
+import { IStackTokens, Stack, TextField } from '@fluentui/react'
 import * as React from 'react'
+import {ExifParserFactory} from 'ts-exif-parser';
+
+import { getErrorAsString } from '../../shared/logging/getErrorAsString'
+
+const childrenTokens: IStackTokens = {
+  childrenGap: 10,
+  padding: 5,
+}
 
 export const AnalyzeImage: React.FC = () => {
-    return <>TODO</>
-};
+  const [selectedFile, setSelectedFile] = React.useState<File | undefined>(undefined);
+  const [parseResult, setParseResult] = React.useState<string>('')
+
+  React.useEffect(() => {
+    getEncodingResult();
+  }, [selectedFile])
+
+  const getEncodingResult = async () => {
+    if (!selectedFile) {
+      return '';
+    }
+
+    try {
+      const data = await selectedFile.arrayBuffer();
+      const parser = ExifParserFactory.create(data);
+      parser.flags.readBinaryTags = true;
+      parser.flags.resolveTagNames = true;
+      parser.flags.returnTags = true;
+
+      const output = parser.parse();
+      // eslint-disable-next-line no-console
+      console.log(output);
+      const set = JSON.stringify(output, undefined, '\t');
+      setParseResult(set);
+    } catch (error: unknown) {
+      const set = getErrorAsString(error)
+      setParseResult(set);
+    }
+
+    return;
+  }
+
+  return (
+    <Stack tokens={ childrenTokens } >
+        <input
+          type="file"
+          onChange={(e) => {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const file = e.target?.files![0];
+            setSelectedFile(file);
+          }}
+        />
+      <TextField
+        label='Parsed image metadata'
+        readOnly={ true }
+        value={ parseResult }
+        multiline={ true }
+        rows={ 40 } />
+    </Stack>
+  )
+}
