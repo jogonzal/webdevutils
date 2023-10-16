674854b2720396e2021571ee81bf6cbe7b7253ec
Jorge <jogonzal@users.noreply.github.com>
2023-10-15 18:12:28 -0700

    Try to fix build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 // @ts-check
- 
+
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   /* config options here */
-}
- 
-module.exports = nextConfig
+  transpilePackages: ["@fluentui/react"],
+};
+
+module.exports = nextConfig;
